0ecf4a827a194d4706b5aba7ec0e9144ac6bb8d2
Jakub Wroński <wronski2002@wp.pl>
2022-11-17 17:24:27 +0100

    Created first version of product page

--- src/views/ProductSection/ProductSection.js
+++ src/views/ProductSection/ProductSection.js
@@ -5,22 +5,39 @@ import Producer from "../../components/atoms/Producer/Producer";
 import Name from "../../components/atoms/Name/Name";
 import Description from "../../components/atoms/Description/Description";
 import Price from "../../components/atoms/Price/Price";
+import AddSymbol from "../../components/atoms/AddSymbol/AddSymbol";
+import RemoveSymbol from "../../components/atoms/RemoveSymbol/RemoveSymbol";
+import { AddButton } from "./ProductSection.styles";
+import { useParams } from "react-router-dom";
+import data from "../../data/data";
 
 const ProductSection = () => {
+  const { id } = useParams();
+
+  const checkProduct = (element) => {
+    const checkFormula = `${element.producer}-${element.id.split("-")[0]}`;
+    if (id === checkFormula) return element;
+  };
+
+  const filteredArray = data.sneakers.filter(checkProduct);
+  const [product] = filteredArray;
+
   return (
-    <Wrapper>
-      <BigImage src="" alt="" />
-      <Producer content="sneaker company" />
-      <Name content="sneaker name" />
-      <Description content="sneaker description" />
-      <Price content="$0.00" />
-      <ButtonsWrapper>
-        <button></button>
-        <p>0</p>
-        <button></button>
-      </ButtonsWrapper>
-      <button></button>
-    </Wrapper>
+    <>
+      <BigImage src={product.photos[0]} alt="" />
+      <Wrapper>
+        <Producer content={product.producer} />
+        <Name content={product.name} />
+        <Description content={product.description} />
+        <Price content={product.price} />
+        <ButtonsWrapper>
+          <AddSymbol />
+          <p>0</p>
+          <RemoveSymbol />
+        </ButtonsWrapper>
+        <AddButton>Add to cart</AddButton>
+      </Wrapper>
+    </>
   );
 };
 
--- src/views/ProductSection/ProductSection.styles.js
+++ src/views/ProductSection/ProductSection.styles.js
@@ -6,8 +6,53 @@ export const Wrapper = styled.div`
 
 export const BigImage = styled.img`
   width: 100%;
-  max-height: 30%;
-  object-fit: contain;
+  max-height: 30vh;
+  object-fit: cover;
+  background-color: orange;
 `;
 
-export const ButtonsWrapper = styled.div``;
+export const ButtonsWrapper = styled.div`
+  margin: 1.4em 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.lightGrayishBlue};
+  border-radius: 15px;
+  overflow: hidden;
+
+  p {
+    font-size: 1.6rem;
+    text-align: center;
+    font-weight: bold;
+  }
+
+  button,
+  p {
+    width: 33%;
+  }
+
+  button {
+    padding: 0.6em;
+    background-color: transparent;
+    border: none;
+    transition: background-color 0.3s;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: ${({ theme }) => theme.colors.grayishBlue};
+  }
+`;
+
+export const AddButton = styled.button`
+  padding: 1em 0;
+  width: 100%;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.orange};
+  border: none;
+  border-radius: 7px;
+  font-weight: bold;
+  cursor: pointer;
+  box-shadow: 0 0 10px 2px rgba(255, 145, 24, 1);
+`;
