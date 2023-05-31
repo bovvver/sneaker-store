317923ea8986c19d659d4a6c1cff0feda624fcf2
Jakub Wroński <wronski2002@wp.pl>
2023-05-31 13:25:23 +0200

    Added unit tests

---
+++ backend/src/test/java/com/github/sneakerstore/unit/controllers/UserControllerTest.java
@@ -0,0 +1,2 @@
+package com.github.sneakerstore.unit.controllers;public class UserControllerTest {
+}
---
+++ backend/src/test/java/com/github/sneakerstore/unit/services/UserServiceTest.java
@@ -0,0 +1,2 @@
+package com.github.sneakerstore.unit.services;public class UserServiceTest {
+}
